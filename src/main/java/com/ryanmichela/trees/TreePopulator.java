ca76c3e30cdb3d55b6ada8216a2409a58bcf109f
Capri205 <seanob451@gmail.com>
2024-11-06 22:59:49 -0500

    Update to 1.21.3 and newer versions of dependencies

--- src/main/java/com/ryanmichela/trees/TreePopulator.java
+++ src/main/java/com/ryanmichela/trees/TreePopulator.java
@@ -28,6 +28,8 @@ import java.util.regex.Pattern;
 import org.bukkit.Chunk;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.Registry;
 import org.bukkit.World;
 import org.bukkit.block.Biome;
 import org.bukkit.block.Block;
@@ -115,53 +117,123 @@ public class TreePopulator extends BlockPopulator {
     return highestBlock.getY();
   }
 
+  // complete list of biomes for reference
+  /*
+  Biome OCEAN = getBiome("ocean");
+  Biome PLAINS = getBiome("plains");
+  Biome DESERT = getBiome("desert");
+  Biome WINDSWEPT_HILLS = getBiome("windswept_hills");
+  Biome FOREST = getBiome("forest");
+  Biome TAIGA = getBiome("taiga");
+  Biome SWAMP = getBiome("swamp");
+  Biome MANGROVE_SWAMP = getBiome("mangrove_swamp");
+  Biome RIVER = getBiome("river");
+  Biome NETHER_WASTES = getBiome("nether_wastes");
+  Biome THE_END = getBiome("the_end");
+  Biome FROZEN_OCEAN = getBiome("frozen_ocean");
+  Biome FROZEN_RIVER = getBiome("frozen_river");
+  Biome SNOWY_PLAINS = getBiome("snowy_plains");
+  Biome MUSHROOM_FIELDS = getBiome("mushroom_fields");
+  Biome BEACH = getBiome("beach");
+  Biome JUNGLE = getBiome("jungle");
+  Biome SPARSE_JUNGLE = getBiome("sparse_jungle");
+  Biome DEEP_OCEAN = getBiome("deep_ocean");
+  Biome STONY_SHORE = getBiome("stony_shore");
+  Biome SNOWY_BEACH = getBiome("snowy_beach");
+  Biome BIRCH_FOREST = getBiome("birch_forest");
+  Biome DARK_FOREST = getBiome("dark_forest");
+  Biome SNOWY_TAIGA = getBiome("snowy_taiga");
+  Biome OLD_GROWTH_PINE_TAIGA = getBiome("old_growth_pine_taiga");
+  Biome WINDSWEPT_FOREST = getBiome("windswept_forest");
+  Biome SAVANNA = getBiome("savanna");
+  Biome SAVANNA_PLATEAU = getBiome("savanna_plateau");
+  Biome BADLANDS = getBiome("badlands");
+  Biome WOODED_BADLANDS = getBiome("wooded_badlands");
+  Biome SMALL_END_ISLANDS = getBiome("small_end_islands");
+  Biome END_MIDLANDS = getBiome("end_midlands");
+  Biome END_HIGHLANDS = getBiome("end_highlands");
+  Biome END_BARRENS = getBiome("end_barrens");
+  Biome WARM_OCEAN = getBiome("warm_ocean");
+  Biome LUKEWARM_OCEAN = getBiome("lukewarm_ocean");
+  Biome COLD_OCEAN = getBiome("cold_ocean");
+  Biome DEEP_LUKEWARM_OCEAN = getBiome("deep_lukewarm_ocean");
+  Biome DEEP_COLD_OCEAN = getBiome("deep_cold_ocean");
+  Biome DEEP_FROZEN_OCEAN = getBiome("deep_frozen_ocean");
+  Biome THE_VOID = getBiome("the_void");
+  Biome SUNFLOWER_PLAINS = getBiome("sunflower_plains");
+  Biome WINDSWEPT_GRAVELLY_HILLS = getBiome("windswept_gravelly_hills");
+  Biome FLOWER_FOREST = getBiome("flower_forest");
+  Biome ICE_SPIKES = getBiome("ice_spikes");
+  Biome OLD_GROWTH_BIRCH_FOREST = getBiome("old_growth_birch_forest");
+  Biome OLD_GROWTH_SPRUCE_TAIGA = getBiome("old_growth_spruce_taiga");
+  Biome WINDSWEPT_SAVANNA = getBiome("windswept_savanna");
+  Biome ERODED_BADLANDS = getBiome("eroded_badlands");
+  Biome BAMBOO_JUNGLE = getBiome("bamboo_jungle");
+  Biome SOUL_SAND_VALLEY = getBiome("soul_sand_valley");
+  Biome CRIMSON_FOREST = getBiome("crimson_forest");
+  Biome WARPED_FOREST = getBiome("warped_forest");
+  Biome BASALT_DELTAS = getBiome("basalt_deltas");
+  Biome DRIPSTONE_CAVES = getBiome("dripstone_caves");
+  Biome LUSH_CAVES = getBiome("lush_caves");
+  Biome DEEP_DARK = getBiome("deep_dark");
+  Biome MEADOW = getBiome("meadow");
+  Biome GROVE = getBiome("grove");
+  Biome SNOWY_SLOPES = getBiome("snowy_slopes");
+  Biome FROZEN_PEAKS = getBiome("frozen_peaks");
+  Biome JAGGED_PEAKS = getBiome("jagged_peaks");
+  Biome STONY_PEAKS = getBiome("stony_peaks");
+  Biome CHERRY_GROVE = getBiome("cherry_grove");
+  */
+
   private boolean isAcceptableBiome(final Biome biome) {
-    return (biome == Biome.FOREST)
-        || (biome == Biome.BIRCH_FOREST)
-        || (biome == Biome.CHERRY_GROVE)
-        || (biome == Biome.SWAMP)
-        || (biome == Biome.MANGROVE_SWAMP)
-        || (biome == Biome.JUNGLE)
-        || (biome == Biome.BADLANDS)
-        || (biome == Biome.DARK_FOREST)
-        || (biome == Biome.TAIGA)
-        || (biome == Biome.SAVANNA);
+	    return (biome == Biome.FOREST)
+	        || (biome == Biome.BIRCH_FOREST)
+	        || (biome == Biome.CHERRY_GROVE)
+	        || (biome == Biome.SWAMP)
+	        || (biome == Biome.MANGROVE_SWAMP)
+	        || (biome == Biome.JUNGLE)
+	        || (biome == Biome.BADLANDS)
+	        || (biome == Biome.DARK_FOREST)
+	        || (biome == Biome.TAIGA)
+	        || (biome == Biome.SAVANNA);
   }
 
   private Biome simplifyBiome(final Biome biome) {
-    switch (biome) {
-      case FOREST:
-      case WINDSWEPT_FOREST:
-      case FLOWER_FOREST:
+
+	  switch (biome.getKey().value().toUpperCase()) {
+
+      case "FOREST":
+      case "WINDSWEPT_FOREST":
+      case "FLOWER_FOREST":
         return Biome.FOREST;
-      case BIRCH_FOREST:
-      case OLD_GROWTH_BIRCH_FOREST:
+      case "BIRCH_FOREST":
+      case "OLD_GROWTH_BIRCH_FOREST":
         return Biome.BIRCH_FOREST;
-      case CHERRY_GROVE:
+      case "CHERRY_GROVE":
         return Biome.CHERRY_GROVE;
-      case SWAMP:
+      case "SWAMP":
         return Biome.SWAMP;
-      case MANGROVE_SWAMP:
+      case "MANGROVE_SWAMP":
     	return Biome.MANGROVE_SWAMP;
-      case JUNGLE:
-      case SPARSE_JUNGLE:
+      case "JUNGLE":
+      case "SPARSE_JUNGLE":
         return Biome.JUNGLE;
-      case DARK_FOREST:
+      case "DARK_FOREST":
         return Biome.DARK_FOREST;
-      case TAIGA:
-      case SNOWY_TAIGA:
+      case "TAIGA":
+      case "SNOWY_TAIGA":
         return Biome.TAIGA;
-      case SAVANNA:
-      case WINDSWEPT_SAVANNA:
-      case SAVANNA_PLATEAU:
+      case "SAVANNA":
+      case "WINDSWEPT_SAVANNA":
+      case "SAVANNA_PLATEAU":
         return Biome.SAVANNA;
-      case BADLANDS:
-      case ERODED_BADLANDS:
-      case WOODED_BADLANDS:
+      case "BADLANDS":
+      case "ERODED_BADLANDS":
+      case "WOODED_BADLANDS":
     	return Biome.BADLANDS;
       default:
         return null;
-    }
+	  }
   }
 
   private boolean treeCanGrow(final Random random) {
